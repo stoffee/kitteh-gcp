604741e3bec3b41fd81c0d64194c79b1df2fd91e
stoffee <chris@roadkill.org>
2018-12-03 19:02:15 -0800

    changes to tings

--- main.tf
+++ main.tf
@@ -73,7 +73,8 @@ data "template_file" "startup_script_vault" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "consul_cluster" {
-  source = "git::git@github.com:hashicorp/terraform-google-consul.git//modules/consul-cluster?ref=v0.3.0"
+  #  source = "git::git@github.com:hashicorp/terraform-google-consul.git//modules/consul-cluster?ref=v0.3.0"
+  source = "git::https://github.com/stoffee/terraform-google-consul.git//modules/consul-cluster?ref=v0.3.0"
 
   gcp_project_id = "${var.gcp_project_id}"
   gcp_region     = "${var.gcp_region}"
