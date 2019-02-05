7d1774df0b7132933313f4eb9e7c7491766771c9
stoffee <chris@roadkill.org>
2019-02-05 01:32:10 -0800

    adding nomad

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -55,7 +55,23 @@ resource "google_compute_instance_template" "vault_private" {
   machine_type         = "${var.machine_type}"
 
   tags                    = ["${concat(list(var.cluster_tag_name), var.custom_tags)}"]
-  metadata_startup_script = "${var.startup_script}"
+ # metadata_startup_script = "${var.startup_script}"
+
+  metadata_startup_script = <<SCRIPT
+    readonly VAULT_TLS_CERT_FILE="/opt/vault/tls/vault.crt.pem"
+    readonly VAULT_TLS_KEY_FILE="/opt/vault/tls/vault.key.pem"
+    /opt/consul/bin/run-consul --client --cluster-tag-name "${var.consul_cluster_tag_name}"
+    /opt/vault/bin/run-vault --gcs-bucket ${var.vault_cluster_tag_name} --tls-cert-file "$VAULT_TLS_CERT_FILE"  --tls-key-file "$VAULT_TLS_KEY_FILE$/opt/vault/tls/vault.key.pem
+    sudo touch /tmp/shitHEAD
+    sudo mkdir -p /test/vault
+    sudo echo -e '[Unit]\nDescription="HashiCorp Vault - A tool for managing secrets"\nDocumentation=https://www.vaultproject.io/docs/\nRequires=network-online.target\nAfter=network-online.target\n\n[Service]\nExecStart=/usr/bin/vault server -config=/test/vault/config.hcl\nExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nKillSignal=SIGINT\nRestart=on-failure\nRestartSec=5\n\n[Install]\nWantedBy=multi-user.target\n' > /lib/systemd/system/vault.service
+    sudo echo -e 'storage "file" {\n  path = "/opt/vault"\n}\n\nlistener "tcp" {\n  address     = "127.0.0.1:8200"\n  tls_disable = 1\n}\n\nseal "gcpckms" {\n  project     = "${var.gcp_project_id}"\n  region      = "${var.keyring_location}"\n  key_ring    = "${var.key_ring}"\n  crypto_key  = "${var.crypto_key}"\n}\n\ndisable_mlock = true\n' > /test/vault/config.hcl
+    sudo chmod 0664 /lib/systemd/system/vault.service
+    sudo echo -e 'alias v="vault"\nalias vualt="vault"\nexport VAULT_ADDR="http://127.0.0.1:8200"\n' > /etc/profile.d/vault.sh
+    source /etc/profile.d/vault.sh
+    sudo systemctl enable vault
+    sudo systemctl start vault
+  SCRIPT
   metadata                = "${merge(map(var.metadata_key_name_for_cluster_size, var.cluster_size), var.custom_metadata)}"
 
   scheduling {
@@ -83,22 +99,6 @@ resource "google_compute_instance_template" "vault_private" {
     email = "${google_service_account.vault_kms_service_account.email}"
     scopes = ["cloud-platform", "compute-rw", "userinfo-email", "storage-ro"]
   }
-
-#  metadata_startup_script = <<SCRIPT
-#    readonly VAULT_TLS_CERT_FILE="/opt/vault/tls/vault.crt.pem"
-#    readonly VAULT_TLS_KEY_FILE="/opt/vault/tls/vault.key.pem"
-#    /opt/consul/bin/run-consul --client --cluster-tag-name "${var.consul_cluster_tag_name}"
-#    /opt/vault/bin/run-vault --gcs-bucket ${var.vault_cluster_tag_name} --tls-cert-file "$VAULT_TLS_CERT_FILE"  --tls-key-file "$VAULT_TLS_KEY_FILE$/opt/vault/tls/vault.key.pem
-#    sudo touch /tmp/shitHEAD
-#    sudo mkdir -p /test/vault
-#    sudo echo -e '[Unit]\nDescription="HashiCorp Vault - A tool for managing secrets"\nDocumentation=https://www.vaultproject.io/docs/\nRequires=network-online.target\nAfter=network-online.target\n\n[Service]\nExecStart=/usr/bin/vault server -config=/test/vault/config.hcl\nExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nKillSignal=SIGINT\nRestart=on-failure\nRestartSec=5\n\n[Install]\nWantedBy=multi-user.target\n' > /lib/systemd/system/vault.service
-#    sudo echo -e 'storage "file" {\n  path = "/opt/vault"\n}\n\nlistener "tcp" {\n  address     = "127.0.0.1:8200"\n  tls_disable = 1\n}\n\nseal "gcpckms" {\n  project     = "${var.gcp_project_id}"\n  region      = "${var.keyring_location}"\n  key_ring    = "${var.key_ring}"\n  crypto_key  = "${var.crypto_key}"\n}\n\ndisable_mlock = true\n' > /test/vault/config.hcl
-#    sudo chmod 0664 /lib/systemd/system/vault.service
-#    sudo echo -e 'alias v="vault"\nalias vualt="vault"\nexport VAULT_ADDR="http://127.0.0.1:8200"\n' > /etc/profile.d/vault.sh
-#    source /etc/profile.d/vault.sh
-#    sudo systemctl enable vault
-#    sudo systemctl start vault
-#  SCRIPT
 
   # Per Terraform Docs (https://www.terraform.io/docs/providers/google/r/compute_instance_template.html#using-with-instance-group-manager),
   # we need to create a new instance template before we can destroy the old one. Note that any Terraform resource on
