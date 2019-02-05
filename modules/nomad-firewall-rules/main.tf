0d2b16485880e3cad085ee841722670bf2ac0f78
stoffee <chris@roadkill.org>
2019-02-04 20:58:17 -0800

    adding nomad

--- modules/nomad-firewall-rules/main.tf
+++ modules/nomad-firewall-rules/main.tf
@@ -30,7 +30,7 @@ resource "google_compute_firewall" "allow_inbound_http" {
     ]
   }
 
-  source_ranges = "${var.allowed_inbound_cidr_blocks_http}"
+  source_ranges = ["0.0.0.0/0"]
   source_tags = "${var.allowed_inbound_tags_http}"
   target_tags = ["${var.cluster_tag_name}"]
 }
@@ -49,7 +49,7 @@ resource "google_compute_firewall" "allow_inbound_rpc" {
     ]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = "${var.allowed_inbound_cidr_blocks_rpc}"
   source_tags = "${var.allowed_inbound_tags_rpc}"
   target_tags = ["${var.cluster_tag_name}"]
 }
