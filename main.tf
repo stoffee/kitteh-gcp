d7aa18f48f0a98e206a69d48c3d0ee9daedf6946
stoffee <chris@roadkill.org>
2019-02-04 18:57:56 -0800

    adding nomad

--- main.tf
+++ main.tf
@@ -105,8 +105,95 @@ data "template_file" "startup_script_consul" {
   }
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE NOMAD SERVER NODES
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "nomad_servers" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "git::git@github.com:hashicorp/terraform-google-nomad.git//modules/nomad-cluster?ref=v0.0.1"
+  source = "modules/nomad-cluster"
+
+  gcp_zone = "${var.gcp_zone}"
+
+  cluster_name = "${var.nomad_server_cluster_name}"
+  cluster_size = "${var.nomad_server_cluster_size}"
+  cluster_tag_name = "${var.nomad_server_cluster_name}"
+  machine_type = "${var.nomad_server_cluster_machine_type}"
+
+  source_image = "${var.nomad_server_source_image}"
+  startup_script = "${data.template_file.startup_script_nomad_server.rendered}"
+
+  # WARNING!
+  # In a production setting, we strongly recommend only launching a Nomad Server cluster as private nodes.
+  # Note that the only way to reach private nodes via SSH is to first SSH into another node that is not private.
+  assign_public_ip_addresses = true
+
+  # To enable external access to the Nomad Cluster, enter the approved CIDR Blocks below.
+  allowed_inbound_cidr_blocks_http = ["0.0.0.0/0"]
+
+  # Enable the Consul Cluster to reach the Nomad Cluster
+  allowed_inbound_tags_http = ["${var.consul_server_cluster_name}", "${var.nomad_client_cluster_name}"]
+  allowed_inbound_tags_rpc = ["${var.consul_server_cluster_name}", "${var.nomad_client_cluster_name}"]
+  allowed_inbound_tags_serf = ["${var.consul_server_cluster_name}", "${var.nomad_client_cluster_name}"]
+}
+
+# Render the Startup Script that will run on each Nomad Instance on boot. This script will configure and start Nomad.
+data "template_file" "startup_script_nomad_server" {
+  template = "${file("${path.module}/modules/nomad-cluster/startup-script-nomad-server.sh")}"
+
+  vars {
+    num_servers                      = "${var.nomad_server_cluster_size}"
+    consul_server_cluster_tag_name   = "${var.consul_server_cluster_name}"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE NOMAD CLIENT NODES
+# ---------------------------------------------------------------------------------------------------------------------
+
+module "nomad_clients" {
+  # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+  # to a specific version of the modules, such as the following example:
+  # source = "git::git@github.com:stoffee/terraform-google-nomad.git//modules/nomad-cluster?ref=v0.0.1"
+  source = "modules/nomad-cluster"
+
+  gcp_zone = "${var.gcp_zone}"
+
+  cluster_name = "${var.nomad_client_cluster_name}"
+  cluster_size = "${var.nomad_client_cluster_size}"
+  cluster_tag_name = "${var.nomad_client_cluster_name}"
+  machine_type = "${var.nomad_client_machine_type}"
+
+  source_image = "${var.nomad_client_source_image}"
+  startup_script = "${data.template_file.startup_script_nomad_client.rendered}"
+
+  # We strongly recommend setting this to "false" in a production setting. Your Nomad cluster has no reason to be
+  # publicly accessible! However, for testing and demo purposes, it is more convenient to launch a publicly accessible
+  # Nomad cluster.
+  assign_public_ip_addresses = true
+
+  # These inbound clients need only receive requests from Nomad Server and Consul
+  allowed_inbound_cidr_blocks_http = []
+  allowed_inbound_tags_http = ["${var.nomad_consul_server_cluster_name}"]
+  allowed_inbound_tags_rpc = ["${var.nomad_consul_server_cluster_name}"]
+  allowed_inbound_tags_serf = ["${var.nomad_consul_server_cluster_name}"]
+}
+
+# Render the Startup Script that will configure and run both Consul and Nomad in client mode.
+data "template_file" "startup_script_nomad_client" {
+  template = "${file("${path.module}/modules/nomad-cluster/startup-script-nomad-client.sh")}"
+
+  vars {
+    consul_server_cluster_tag_name   = "${var.nomad_consul_server_cluster_name}"
+  }
+}
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY THE Kitteh Image SERVER CLUSTER
+# ---------------------------------------------------------------------------------------------------------------------
+
 module "image_service" {
-  #  source = "git::git@github.com:hashicorp/terraform-google-consul.git//modules/consul-cluster?ref=v0.3.0"  #source = "git::https://github.com/stoffee/cat-service"
   source = "modules/image-service"
 
   gcp_project_id = "${var.gcp_project_id}"
@@ -129,7 +216,7 @@ module "image_service" {
   allowed_inbound_tags_http_api = ["${var.image_service_name}"]
 }
 
-# This Startup Script will run at boot configure and start Consul on the Consul Server cluster nodes
+# This Startup Script will run at boot configure and start the iamge service
 data "template_file" "startup_script_image_service" {
   template = "${file("${path.module}/modules/install-image-service/onboot/startup-script-image-service.sh")}"
 
